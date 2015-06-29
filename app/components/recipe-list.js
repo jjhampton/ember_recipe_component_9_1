c6c7d54c5f7b139c0c4d3865d04c5508b663ae51
John Hampton <john_j_hampton@yahoo.com>
2015-06-29 17:44:15 -0400

    Move recipe data to component

--- app/components/recipe-list.js
+++ app/components/recipe-list.js
@@ -2,5 +2,42 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'ul',
-  classNames: ['recipe-list']
+  classNames: ['recipe-list'],
+  items: [
+    {
+      quantity: "1",
+      unit: "pound",
+      name: "Boneless chicken breast"
+    },
+    {
+      quantity: "1",
+      unit: "cup",
+      name: "Sliced carrots"
+    },
+    {
+      quantity: "1",
+      unit: "cup",
+      name: "Frozen green peas"
+    },
+    {
+      quantity: "1/2",
+      unit: "cup",
+      name: "Sliced celery"
+    },
+    {
+      quantity: "1/3",
+      unit: "cup",
+      name: "Butter"
+    },
+    {
+      quantity: "1/3",
+      unit: "cup",
+      name: "Chopped onion"
+    },
+    {
+      quantity: "1/3",
+      unit: "cup",
+      name: "All-purpose flour"
+    }
+  ]
 });
