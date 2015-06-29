dff9839586b282de4dc503ebede21eda26b074e5
John Hampton <john_j_hampton@yahoo.com>
2015-06-29 17:50:03 -0400

    Move data to model hook in the route instead of in the component .js file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return [
+      {
+        quantity: "1",
+        unit: "pound",
+        name: "Boneless chicken breast"
+      },
+      {
+        quantity: "1",
+        unit: "cup",
+        name: "Sliced carrots"
+      },
+      {
+        quantity: "1",
+        unit: "cup",
+        name: "Frozen green peas"
+      },
+      {
+        quantity: "1/2",
+        unit: "cup",
+        name: "Sliced celery"
+      },
+      {
+        quantity: "1/3",
+        unit: "cup",
+        name: "Butter"
+      },
+      {
+        quantity: "1/3",
+        unit: "cup",
+        name: "Chopped onion"
+      },
+      {
+        quantity: "1/3",
+        unit: "cup",
+        name: "All-purpose flour"
+      }
+    ];
+  }
 });
