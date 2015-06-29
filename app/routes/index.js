3440fce3ff52ed56a71450bc3e01c740d1399318
John Hampton <john_j_hampton@yahoo.com>
2015-06-29 18:58:40 -0400

    Add double unit test

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,37 +4,37 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        quantity: "1",
+        quantity: 1,
         unit: "pound",
         name: "Boneless chicken breast"
       },
       {
-        quantity: "1",
+        quantity: 1,
         unit: "cup",
         name: "Sliced carrots"
       },
       {
-        quantity: "1",
+        quantity: 1,
         unit: "cup",
         name: "Frozen green peas"
       },
       {
-        quantity: "1/2",
+        quantity: 1/2,
         unit: "cup",
         name: "Sliced celery"
       },
       {
-        quantity: "1/3",
+        quantity: 1/3,
         unit: "cup",
         name: "Butter"
       },
       {
-        quantity: "1/3",
+        quantity: 1/3,
         unit: "cup",
         name: "Chopped onion"
       },
       {
-        quantity: "1/3",
+        quantity: 1/3,
         unit: "cup",
         name: "All-purpose flour"
       }
---
+++ tests/acceptance/double-quantity-test.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from 'ember-recipe-component-9-1/tests/helpers/start-app';
+
+var application;
+
+module('Acceptance | double quantity', {
+  beforeEach: function() {
+    application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(application, 'destroy');
+  }
+});
+
+test('clicking Adjust Recipe button should double quantity', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(find('.quantity').get(0).textContent, "2");
+    click('.adjust recipe');
+  });
+
+  andThen(function() {
+    assert.equal(find('.quantity').get(3).textContent, "1");
+    click('.adjust-recipe');
+  });
+
+  andThen(function() {
+    assert.equal(find('.quantity').get(5).textContent, "0.6666666666666666");
+    click('.adjust-recipe');
+  });
+
+});
