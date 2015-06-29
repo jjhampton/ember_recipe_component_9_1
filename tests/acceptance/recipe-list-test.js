df72abe2245bc610cb0cecae4dc067b9d35f47d5
John Hampton <john_j_hampton@yahoo.com>
2015-06-29 17:18:01 -0400

    Move list to index.hbs

--- tests/acceptance/recipe-list-test.js
+++ tests/acceptance/recipe-list-test.js
@@ -20,21 +20,29 @@ test('visiting index lists the recipe items', function(assert) {
   andThen(function() {
     assert.equal(find('.recipe-item').length, 7);
 
-    // assert.equal(find('.recipe-item .quantity').get(0).textContent, "1");
-    // assert.equal(find('.recipe-item .quantity').get(1).textContent, "1");
-    // assert.equal(find('.recipe-item .quantity').get(2).textContent, "1");
-    // assert.equal(find('.recipe-item .quantity').get(3).textContent, "1/2");
-    // assert.equal(find('.recipe-item .quantity').get(4).textContent, "1/3");
-    // assert.equal(find('.recipe-item .quantity').get(5).textContent, "1/3");
-    // assert.equal(find('.recipe-item .quantity').get(6).textContent, "1/3");
-    // assert.equal(find('.recipe-item .quantity').get(6).textContent, "1/3");
-    //
-    // assert.equal(find('.recipe-item .name').get(0).textContent, "Boneless chicken breast");
-    // assert.equal(find('.recipe-item .name').get(1).textContent, "Sliced carrots");
-    // assert.equal(find('.recipe-item .name').get(2).textContent, "Frozen green peas");
-    // assert.equal(find('.recipe-item .name').get(3).textContent, "Sliced celery");
-    // assert.equal(find('.recipe-item .name').get(4).textContent, "Butter");
-    // assert.equal(find('.recipe-item .name').get(5).textContent, "Chopped onion");
-    // assert.equal(find('.recipe-item .name').get(6).textContent, "All-purpose flour");
+    assert.equal(find('.recipe-item .quantity').get(0).textContent, "1");
+    assert.equal(find('.recipe-item .quantity').get(1).textContent, "1");
+    assert.equal(find('.recipe-item .quantity').get(2).textContent, "1");
+    assert.equal(find('.recipe-item .quantity').get(3).textContent, "1/2");
+    assert.equal(find('.recipe-item .quantity').get(4).textContent, "1/3");
+    assert.equal(find('.recipe-item .quantity').get(5).textContent, "1/3");
+    assert.equal(find('.recipe-item .quantity').get(6).textContent, "1/3");
+    assert.equal(find('.recipe-item .quantity').get(6).textContent, "1/3");
+
+    assert.equal(find('.recipe-item .name').get(0).textContent, "Boneless chicken breast");
+    assert.equal(find('.recipe-item .name').get(1).textContent, "Sliced carrots");
+    assert.equal(find('.recipe-item .name').get(2).textContent, "Frozen green peas");
+    assert.equal(find('.recipe-item .name').get(3).textContent, "Sliced celery");
+    assert.equal(find('.recipe-item .name').get(4).textContent, "Butter");
+    assert.equal(find('.recipe-item .name').get(5).textContent, "Chopped onion");
+    assert.equal(find('.recipe-item .name').get(6).textContent, "All-purpose flour");
+
+    assert.equal(find('.recipe-item .unit').get(0).textContent, "pound");
+    assert.equal(find('.recipe-item .unit').get(1).textContent, "cup");
+    assert.equal(find('.recipe-item .unit').get(2).textContent, "cup");
+    assert.equal(find('.recipe-item .unit').get(3).textContent, "cup");
+    assert.equal(find('.recipe-item .unit').get(4).textContent, "cup");
+    assert.equal(find('.recipe-item .unit').get(5).textContent, "cup");
+    assert.equal(find('.recipe-item .unit').get(6).textContent, "cup");
   });
 });
